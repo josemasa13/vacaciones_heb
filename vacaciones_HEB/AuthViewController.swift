3d93c520b33c1794ee585196bf1e19dcb9502100
Ricardo Ramierz <ricardo.rmz.97@hotmail.com>
2020-04-22 21:08:05 -0500

    login se dirige a interfaces particulares

--- vacaciones_HEB/AuthViewController.swift
+++ vacaciones_HEB/AuthViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 
 class AuthViewController: UIViewController {
+    
+    let db = Firestore.firestore()
+    var id: String!
 
     @IBOutlet weak var tfUsuario: UITextField!
     @IBOutlet weak var tfContraseña: UITextField!
@@ -24,13 +28,38 @@ class AuthViewController: UIViewController {
             Auth.auth().signIn(withEmail: usuario, password: contraseña){ (result, error) in
                 if let result = result, error == nil {
                     // show next page
-                    print("->>>>  FUNCIONA")
+                    print("\(result.user.uid)->>>>  FUNCIONA")
+                    self.id = result.user.uid
+                    self.db.collection("users").document(result.user.uid).addSnapshotListener() { (snapshot, error) in
+                        if error == nil {
+                            if (snapshot?.get("reportaA") as! String == "" ) {
+                                print("ES ADMIN")
+                                self.performSegue(withIdentifier: "admin", sender: nil)
+                            }else{
+                                print("ES EMPLEADO")
+                                self.performSegue(withIdentifier: "user", sender: nil)
+                            }
+                        }
+                        }
+                    
                 }else {
                     let alert = UIAlertController(title: "Error", message: "Error al iniciar sesion", preferredStyle: .alert)
                     alert.addAction(UIAlertAction(title: "Aceptar", style: .default))
                     self.present(alert, animated: true, completion: nil)
                 }
             }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "admin" {
+            let nav = segue.destination as! UINavigationController
+            let vc = nav.topViewController as! AdminTableViewController
+            vc.userID = self.id
+        }else {
+            let nav = segue.destination as! UINavigationController
+            let vc = nav.topViewController as! EmployeeViewController
+            vc.userID = self.id
         }
     }
     
